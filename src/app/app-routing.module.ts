12320ab0b2e0325acdedcb4e6524b19e6c78d986
Maria Gabriela Suarez <maria.suarez@comafi.com.ar>
2021-06-23 19:26:54 -0300

    configuracion de angular como single-spa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularComponent } from './angular/angular.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'angular', component: AngularComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }]
 })
 export class AppRoutingModule { }
